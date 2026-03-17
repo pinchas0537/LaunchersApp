8e5ef2dcac456fe18c75e2c7ee26cb281f3ed1c1
Pinchas Lifshitz <pnl8480537@gmail.com>
2026-03-17 18:59:22 +0200

    added updeate user

--- client/src/pages/AllUsers.jsx
+++ client/src/pages/AllUsers.jsx
@@ -4,9 +4,11 @@ import { isAxiosError } from "axios"
 import { useNavigate } from "react-router"
 import Navbar from "../component/Navbar"
 import { allUsers } from "../api/authApi"
+import UpdeteUser from "../component/updeteUser"
 export default function AllUsers() {
     const token = localStorage.getItem("token")
     const [usersArr, setUsers] = useState([])
+    const [updaete, setUpdate] = useState(false)
     const [error, setError] = useState("")
     const navigate = useNavigate()
     async function users() {
@@ -27,18 +29,27 @@ export default function AllUsers() {
     return (
         <div className="home">
             <Navbar />
-            {usersArr.map((launcher) => (
-                    <div key={launcher._id}>
-                        <button className="card" onClick={() => {
-                            navigate(`/launcherdetails/${launcher._id}`)
-                        }}>
-                            <p><strong>ID:</strong>{usersArr._id}</p>
-                            <p><strong>username:</strong>{usersArr.username}</p>
-                        <p><strong>סוג המשגר:</strong>{ודקocketType}</p>
-                            <p><strong>האם הושמד:</strong>{launcher.Wasitdestroyed}</p>
-                        </button>
-                    </div>
-                ))}
+            {usersArr.map((user) => (
+                <div key={user._id}>
+                    <button className="users" onClick={() => {
+                        navigate(`/launcherdetails/${launcher._id}`)
+                    }}>
+                        <p><strong>ID:</strong>{user._id}</p>
+                        <p><strong>username:</strong>{user.username}</p>
+                        <p><strong>password:</strong>{user.password}</p>
+                        <p><strong>email:</strong>{user.email}</p>
+                        <p><strong>user_type:</strong>{user.user_type}</p>
+                        <p><strong>last_login:</strong>{user.last_login}</p>
+                    <button onClick={() => {
+                        setUpdate(true)
+                    }}>update user</button>
+                    </button>
+                    
+                    {updaete === true ? (<>
+                        <UpdeteUser cancellation={() => setUpdate(false)} />
+                    </>) : ""}
+                </div>
+            ))}
             <div className="error">
                 {error}
             </div>
